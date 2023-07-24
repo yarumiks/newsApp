696c6f2eb063519856bfeef0a68f962e5d268340
yasar <yasarkursun99@gmail.com>
2023-07-24 17:51:24 +0300

    delete component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { CategoriesComponent } from './components/categories/categories.component';
+
 import { HomeComponent } from './components/home/home.component';
 import { HttpClientModule, HttpClient} from '@angular/common/http';
-import { CommonModule } from '@angular/common';
+
 @NgModule({
   declarations: [
     AppComponent,
-    CategoriesComponent,
     HomeComponent
   ],
   imports: [
